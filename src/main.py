bc0d72cff13e1812486230bbb514230807495e7e
paolo.bevilacqua <paolo.bevilacqua@soft.it>
2023-02-01 22:34:00 +0100

    merge with master

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.recogniser.wbb_recogniser import WBBRecogniser
 def main():
     wbb_recogniser = WBBRecogniser()
 
-    wbb_recogniser.run_test(pathname="../data/Test/Walk_Ex_mavi_1")
+    wbb_recogniser.run_test(pathname="../data/Test/Walk_Ex_maestro_1mesedopo")
 
 
 if __name__ == "__main__":
