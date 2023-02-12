b08a197e620753eab715af4c90297b927116a360
Ballants <cballanti99@gmail.com>
2023-02-12 18:03:10 +0100

    Added Identification demo and minor fixes

--- src/demo/demo.py
+++ src/demo/demo.py
@@ -53,14 +53,73 @@ class IdentityVerifier:
         return min_distance <= self.at
 
 
+class SubjectIdentifier:
+    def __init__(self, wbb_recognizer: WBBRecogniser, acceptance_threshold: float = 4.94):
+        self.at = acceptance_threshold
+        self.wbb_recognizer = wbb_recognizer
+
+    def identify(self, pathname, maxRank: int = 1):
+        # Read sample data from file
+        sample = np.array(np.loadtxt(pathname, dtype=float))
+
+        # features extraction from time series
+        ts = self.wbb_recognizer.normalize_sample_to_timeseries(sample, id="demo_sample", counter=1)
+        probe_features = self.wbb_recognizer.extract_features_from_timeseries(ts)
+
+        # scale features with fitted scaler
+        scaled_features = self.wbb_recognizer.scaler.transform(probe_features["template"])
+
+        # filter the calculated filter
+        df = pd.DataFrame(scaled_features)
+        df.columns = probe_features["features_name"]
+
+        filtered_features_sample = df[self.wbb_recognizer.features_name].to_numpy()
+
+        """ Compute distances between probe's template and all the other templates """
+
+        gallery_template = self.wbb_recognizer.extracted_features
+
+        # filter gallery_template with the current features list
+        gallery_template = gallery_template[self.wbb_recognizer.features_name]
+
+        # no user found with that claimed id
+        if len(df) == 0:
+            return []
+
+        # Compute distance with every template associated to that claimed id
+        distances = []
+        for idx, row in enumerate(gallery_template.to_numpy()):
+            row_features = row.reshape(1, -1)
+            stacked = np.vstack((row_features, filtered_features_sample.reshape(1, -1)))
+            curr_label = self.wbb_recognizer.extracted_features.index[idx]
+            distances += [(pdist(stacked, metric='euclidean')[0], curr_label)]
+
+        distances.sort(key=lambda el: el[0])
+        if type(self.at) == type(float()):
+            distances = list(filter(lambda x: x[0] < self.at, distances))
+        return distances[:maxRank]
+
+
 if __name__ == '__main__':
 
     wbb_recognizer = WBBRecogniser()
 
-    identity_verifier = IdentityVerifier(wbb_recognizer)
-    claimed_identity = "CHIARA_F_23_DX_S"
+    x = "i"
 
-    if identity_verifier.verify(config.TEST_DEMO_PATH, claimed_identity):
-        print(f"Identity verified, you are user #{claimed_identity}")
+    if x == "v":
+        identity_verifier = IdentityVerifier(wbb_recognizer)
+        claimed_identity = "CHIARA_F_23_DX_S"
+
+        if identity_verifier.verify(config.TEST_DEMO_PATH, claimed_identity):
+            print(f"Identity verified, you are user {claimed_identity}")
+        else:
+            print("Access denied.")
+
     else:
-        print("Access denied.")
+        subjectIdentifier = SubjectIdentifier(wbb_recognizer)
+
+        users = subjectIdentifier.identify(config.TEST_DEMO_PATH)
+        if len(users) == 0:
+            print(f"No match in the dataset")
+        else:
+            print(f"You are user {users[0][1]}")
